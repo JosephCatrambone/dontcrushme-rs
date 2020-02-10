4cb7941276e757629eafd6442073a9d0dc23261d
Joseph Catrambone <jo.jcat@gmail.com>
2020-02-09 21:35:47 -0800

    Convert to Godot library. Prep for move to pre-baked hashmap.

--- src/lib.rs
+++ src/lib.rs
@@ -1,55 +1,20 @@
 #[macro_use]
 extern crate approx; // For the macro relative_eq!
+//extern crate phf;
 
+use gdnative::*;
 use hashbrown::{HashMap, HashSet};
-use std::io::prelude::*;
-use std::io::BufReader;
-use std::io::SeekFrom;
-use std::fs::File;
 use nalgebra;
-use typenum::U50;
 use nalgebra::Vector;
+//use phf::phf_map;
+use std::io::prelude::*;
+use std::io::{BufReader, BufWriter};
+use std::io::SeekFrom;
 use std::iter::Zip;
+use std::fs::File;
+use typenum::U50;
 
-trait WordVectorizer {
-	fn word_in_vocabulary(&self, word: &str) -> bool;
-	fn vectorize(&self, word: &str) -> Vec<f32>;
-}
-
-struct GloveInMem {
-	word_to_vector: HashMap<String, Vec<f32>>
-}
-
-impl GloveInMem {
-	fn new(glove_vectors: &str) -> Self {
-		let mut fin = File::open(glove_vectors).expect(format!("Can't load vectors from '{}'", glove_vectors).as_str());
-		let mut buffered_fin = BufReader::new(fin);
-		let mut word_to_vector = HashMap::with_capacity(400_000);
-		
-		for line_result in buffered_fin.lines() {
-			if let Ok(line) = line_result {
-				let mut tokens = line.split_whitespace();
-				let word = tokens.next().unwrap().to_string();
-				let digits: Vec<f32> = tokens.map(|x| { x.parse::<f32>().unwrap() }).collect();
-				word_to_vector.insert(word, digits);
-				//nalgebra::VectorN::from_data(digits);
-			}
-		}
-		
-		GloveInMem { word_to_vector }
-	}
-}
-
-impl WordVectorizer for GloveInMem {
-	fn word_in_vocabulary(&self, word: &str) -> bool {
-		self.word_to_vector.contains_key(word)
-	}
-	
-	fn vectorize(&self, word: &str) -> Vec<f32> {
-		self.word_to_vector.get(word).unwrap().clone()
-	}
-}
-
+// Utility methods
 fn cosine_similarity(va: &Vec<f32>, vb: &Vec<f32>) -> f32 {
 	let mut accumulator = 0.0f32;
 	let mut magnitude_a = 0.0f32;
@@ -64,14 +29,88 @@ fn cosine_similarity(va: &Vec<f32>, vb: &Vec<f32>) -> f32 {
 	accumulator / (magnitude_a.sqrt() * magnitude_b.sqrt())
 }
 
+/// The WordVectorizer "class"
+#[derive(NativeClass)]
+#[inherit(Node)]
+#[user_data(user_data::ArcData<WordVectorizer>)]
+pub struct WordVectorizer {
+	word_to_vector: HashMap<String, Vec<f32>>
+}
+
+// __One__ `impl` block can have the `#[methods]` attribute, which will generate
+// code to automatically bind any exported methods to Godot.
+#[methods]
+impl WordVectorizer {
+	/// The "constructor" of the class.
+	fn _init(_owner: Node) -> Self {
+		WordVectorizer {
+			word_to_vector: HashMap::new()
+		}
+	}
+
+	// In order to make a method known to Godot, the #[export] attribute has to be used.
+	// In Godot script-classes do not actually inherit the parent class.
+	// Instead they are"attached" to the parent object, called the "owner".
+	// The owner is passed to every single exposed method.
+	#[export]
+	fn _ready(&self, _owner: Node) {
+		// The `godot_print!` macro works like `println!` but prints to the Godot-editor
+		// output tab as well.
+		godot_print!("Loading vectors.");
+	}
+
+	#[export]
+	fn similarity(&self, _owner:Node, s1:GodotString, s2:GodotString) -> Variant {
+		let v1 = self.vectorize(&s1.to_string());
+		let v2 = self.vectorize(&s2.to_string());
+		return Variant::from_f64(cosine_similarity(&v1, &v2) as f64);
+	}
+
+	fn from_text_file(glove_vectors: &str) -> Self {
+		let mut fin = File::open(glove_vectors).expect(format!("Can't load vectors from '{}'", glove_vectors).as_str());
+		let mut buffered_fin = BufReader::new(fin);
+		let mut word_to_vector = HashMap::with_capacity(400_000);
+
+		for line_result in buffered_fin.lines() {
+			if let Ok(line) = line_result {
+				let mut tokens = line.split_whitespace();
+				let word = tokens.next().unwrap().to_string();
+				let digits: Vec<f32> = tokens.map(|x| { x.parse::<f32>().unwrap() }).collect();
+				word_to_vector.insert(word, digits);
+				//nalgebra::VectorN::from_data(digits);
+			}
+		}
+
+		WordVectorizer { word_to_vector }
+	}
+
+	fn word_in_vocabulary(&self, word: &str) -> bool {
+		self.word_to_vector.contains_key(word)
+	}
+
+	fn vectorize(&self, word: &str) -> Vec<f32> {
+		self.word_to_vector.get(word).unwrap().clone()
+	}
+}
+
+// Function that registers all exposed classes to Godot
+fn init(handle: gdnative::init::InitHandle) {
+	handle.add_class::<WordVectorizer>();
+}
+
+// macros that create the entry-points of the dynamic library.
+godot_gdnative_init!();
+godot_nativescript_init!(init);
+godot_gdnative_terminate!();
+
 
 #[cfg(test)]
 mod tests {
-	use crate::{WordVectorizer, GloveInMem, cosine_similarity};
+	use crate::{WordVectorizer, cosine_similarity};
 	
 	#[test]
 	fn sanity_check_word_similarity() {
-		let wv = GloveInMem::new("glove.6B.50d.txt");
+		let wv = WordVectorizer::from_text_file("glove.6B.50d.txt");
 		let wv1 = wv.vectorize("cat");
 		let wv2 = wv.vectorize("feline");
 		let wv3 = wv.vectorize("eggplant");
